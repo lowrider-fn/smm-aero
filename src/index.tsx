cf0a9ef7285d90233254492a0b429b26ddc6fc7f
Dolmatsky-SeVy <Dolmatsky-SeVy@rshbext.ru>
2022-05-19 19:24:29 +0300

    eject

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 const Router = (process.env.NODE_ENV === 'production' && HashRouter) || BrowserRouter;
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('app')
-);
+createRoot(document.getElementById('app')!)
+  .render(
+    <Router>
+      <App />
+    </Router>
+  );
